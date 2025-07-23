b3c5bc4456e9453a6e52ed11adbe33786994aa72
kevin <oropezavazquezk58@gmail.com>
2025-07-23 01:19:01 -0700

    hero section-test

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
-
+import Button from "@/UI/Buttons/Button";
+import "./Home.scss";
 
 export const metadata = {
   title: 'Home page Landscaping title',
@@ -7,8 +8,17 @@ export const metadata = {
 }
 
 function Home() {
-  return (
-      <h1>Hello world </h1>
+  return ( 
+    // Hero section home page
+    <div className="hero-container"> 
+      <div className="hero-content">
+        <h1>Tittle where we adress the <span className="highlight"> main pain point</span></h1>
+        <p>Description where we adress how we can resolve the main pain point main pain point that the client might have. Proving our value and being apart of our competitors.</p>
+        <Button text="Adress pain point" size="medium" link="/" variant="filled" textColor="#28282A" backgroundColor="#FCFAF8" iconColor="#F1572D" />
+        <Button text="Check our projects" size="medium" link="/" variant="minimal" textColor="#FCFAF8" backgroundColor="transparent" />
+     </div>
+    </div>
+
   )
 }
 
