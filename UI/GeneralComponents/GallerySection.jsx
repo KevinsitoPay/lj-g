bd5345666917a2069920b726c028914755e9f0a5
kevin <oropezavazquezk58@gmail.com>
2025-08-06 17:19:52 -0700

    TEST

--- UI/GeneralComponents/GallerySection.jsx
+++ UI/GeneralComponents/GallerySection.jsx
@@ -110,8 +110,8 @@ const GallerySection = () => {
           >
             {selectedService.images.concat(selectedService.images).map((image, index) => (
               <div className={styles.imageWrapper} key={index}>
-                {console.log(`/images/${image}`)} {/* Imprime la ruta para verificar */}
-                <img src={`/images/${image}`} alt={selectedService.name} />
+                {console.log(`/Images/${image}`)} {/* Imprime la ruta para verificar */}
+                <img src={`/Images/${image}`} alt={selectedService.name} />
               </div>
             ))}
           </div>
