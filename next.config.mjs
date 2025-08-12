605e5c0b521151b3e0e7863692a85b04229f9ab5
kevin <oropezavazquezk58@gmail.com>
2025-08-12 14:20:43 -0700

    test path vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pages: [
+    {
+      path: '/contractors',
+      page: '/contractors/index',
+    },
+    {
+      path: '/faq',
+      page: '/faq/index',
+    },
+    {
+      path: '/how-it-works',
+      page: '/how-it-works/index',
+    },
+  ],
+};
 
 export default nextConfig;
