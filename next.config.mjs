f5d8d309b0bf705ef759a4181ec84c14c74947f9
kevin <oropezavazquezk58@gmail.com>
2025-08-12 14:43:17 -0700

    regreso de ajustes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pages: [
-    {
-      path: '/contractors',
-      page: '/contractors/index',
-    },
-    {
-      path: '/faq',
-      page: '/faq/index',
-    },
-    {
-      path: '/how-it-works',
-      page: '/how-it-works/index',
-    },
-  ],
-};
+const nextConfig = {};
 
 export default nextConfig;
