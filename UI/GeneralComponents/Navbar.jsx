f6871635425c41cd7d0d70f77d8121726a4cc118
MonetteDev <lic.andresmichelglez@gmail.com>
2025-08-14 16:36:00 -0700

    Update of header styling three

--- UI/GeneralComponents/Navbar.jsx
+++ UI/GeneralComponents/Navbar.jsx
@@ -7,6 +7,12 @@ import LJLogo from "./LJLogo";
 import ChevronDown from "../Icons/ChevronDown";
 import ArrowIcon from "../Icons/ArrowIcon";
 import LandLeveling from "../Icons/LandLeveling";
+import SodInstallation from "../Icons/SodInstallation";
+import FunctionalPlants from "../Icons/FunctionalPlants";
+import MulchApplication from "../Icons/MulchApplication";
+import RiverRock from "../Icons/RiverRock";
+import WallRetaining from "../Icons/WallRetaining";
+import IrrigationSystem from "../Icons/IrrigationSystem";
 
 function Navbar() {
   const [showServices, setShowServices] = React.useState(false);
@@ -65,7 +71,7 @@ function Navbar() {
         >
           <div className="megamenu-content">
             {/* Column 1 */}
-            <div className="megamenu-col1">
+            <div className="megamenu-col1  ">
               <span>
                 <h4>This is our services</h4>
                 <p>
@@ -74,46 +80,96 @@ function Navbar() {
                 </p>
               </span>
 
-
               <div className="services-list">
-                <h6 className="heading-icon"><LandLeveling /> Land leveling</h6>
-                <p>service description</p>
-                <Link href="/">
-                  Learn more <ArrowIcon size="sm" />
+                {/* Land Leveling */}
+                <Link href={"/land-leveling"} className="service-card">
+                  <h6 className="heading-icon">
+                    <LandLeveling /> Land Leveling
+                  </h6>
+                  <p>
+                    Prevents water pooling, protects foundations, and ensures a
+                    stable base for landscaping or construction.
+                  </p>
+                </Link>
+
+                {/* Sod Installation */}
+                <Link href={"/sod-installation"} className="service-card">
+                  <h6 className="heading-icon">
+                    <SodInstallation /> Sod Installation
+                  </h6>
+                  <p>
+                    Gives you an instant, healthy lawn that is easy to maintain
+                    and looks great in any setting.
+                  </p>
+                </Link>
+
+                {/* Functional Plants */}
+                <Link href={"/functional-plants"} className="service-card">
+                  <h6 className="heading-icon">
+                    <FunctionalPlants /> Functional Plants
+                  </h6>
+                  <p>
+                    Creates beautiful, low maintenance gardens that add value to
+                    your property and enhance the overall aesthetic.
+                  </p>
+                </Link>
+
+                {/* Mulch Application */}
+                <Link href={"/mulch-application"} className="service-card">
+                  <h6 className="heading-icon">
+                    <MulchApplication /> Mulch Application
+                  </h6>
+                  <p>
+                    Helps soil retain moisture, blocks weeds, and enhances the
+                    look of your landscape.
+                  </p>
+                </Link>
+
+                {/* River Rock Installation */}
+                <Link
+                  href={"/river-rock-installation"}
+                  className="service-card"
+                >
+                  <h6 className="heading-icon">
+                    <RiverRock /> River Rock Installation
+                  </h6>
+                  <p>
+                    Controls erosion, improves drainage, and adds natural
+                    beauty.
+                  </p>
+                </Link>
+
+                {/* Wall Retaining Installation */}
+                <Link
+                  href={"/wall-retaining-installation"}
+                  className="service-card"
+                >
+                  <h6 className="heading-icon">
+                    <WallRetaining /> Wall Retaining Installation
+                  </h6>
+                  <p>
+                    Stabilizes sloped areas, prevents soil loss, and ensures a
+                    secure foundation.
+                  </p>
+                </Link>
+
+                {/* Irrigation System Installation */}
+                <Link
+                  href={"/irrigation-system-installation"}
+                  className="service-card"
+                >
+                  <h6 className="heading-icon">
+                    <IrrigationSystem /> Irrigation System Installation
+                  </h6>
+                  <p>
+                    Delivers water efficiently, saves time, and ensures a
+                    healthy garden.
+                  </p>
                 </Link>
               </div>
-            
-
-
-
-
-
-
-
-
-
-
-
-
-            </div>
-
-            {/* Column 2 */}
-            <div className="megamenu-col2">
-              This is the image
             </div>
           </div>
         </div>
-
-
-
-
-
-
-
-
-
-
-
       </div>
     </>
   );
